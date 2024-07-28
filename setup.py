69f2f2000748707392ce30a06b578b648413126f
ELEKTRO_TURBO_DZIK <109021616+warriorCavt@users.noreply.github.com>
2024-07-28 17:14:41 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiotube-shorts',
+    version='1.7.3b',
+    description='A fork of a library to access YouTube Public Data without YouTubeAPI, modified to access Shorts.',
+    author='warriorCavt,
+    author_email='miscwindows@gmail.com,
+    packages=find_packages(include=['aiotube_shorts', 'aiotube_shorts.*']),
+    install_requires=[
+        
+    ],
+)
